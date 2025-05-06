36e899d343560f94827bc6aafa41d4d7fafa8f6a
essamtobgi <essam@genfusion.ai>
2025-05-06 16:09:13 +0300

    Users interactions migrations

---
+++ db/migrate/20250506112223_create_reacts.rb
@@ -0,0 +1,11 @@
+class CreateReacts < ActiveRecord::Migration[8.0]
+  def change
+    create_table :reacts do |t|
+      t.string :name
+      t.belongs_to :user, null: false, foreign_key: true
+      t.belongs_to :property, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
